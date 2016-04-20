e3ad750c47042693b96a1def3197ecc7107f3c0d
Ben Jo <oebbler@live.de>
2016-04-20 07:24:25 +0200

    Build scripts: Toolchain changed to GCC 5.3.1 Linaro

--- bbuild.sh
+++ bbuild.sh
@@ -13,7 +13,7 @@
 BOEFFLA_VERSION="2.8-CM11.0-NG-i9300"
 EXTENDED_CMDLINE=""
 
-TOOLCHAIN="/opt/toolchains/arm-eabi-4.7/bin/arm-eabi-"
+TOOLCHAIN="/opt/toolchains/gcc-linaro-5.3-2016.02-x86_64_arm-eabi/bin/arm-eabi-"
 
 COMPILE_DTB="n"
 MODULES_IN_SYSTEM="y"
